dcbbb89463641ae57dd35145ddd9d8f0abb56230
Carlos Michael Rodriguez <cmr624@nyu.edu>
2019-04-13 15:58:00 -0400

    fixed FPS bugs

--- Assets/DemoSceneCore/Scripts/CameraController.cs
+++ Assets/DemoSceneCore/Scripts/CameraController.cs
@@ -5,32 +5,52 @@ using UnityEngine;
 //Controls the Camera based on rotation
 public class CameraController : MonoBehaviour {
 
+    public Camera cam;
     private Rigidbody rb;
     //the scale at which the speed of the camera moves
-    private float lookSensitivity;// = 2.0f;
-    private int invertY;
+    public float xLookSensitivity;// = 2.0f;
+    public float yLookSensitivity;// = 2.0f;
+
+    public bool invertY;
+
+    public float MinimumX = -90F;
+    public float MaximumX = 90F;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        lookSensitivity = 3.0f;//PlayerPrefs.GetFloat("CameraSensitivity");
-        invertY = -1;//PlayerPrefs.GetInt("InvertY");
-
     }
     // Update is called once per frame
     public void Update () {
-        //brackeys code, basically calculates rotation based on the x / y mouse axis
-        float xRot = Input.GetAxisRaw("Mouse X");
-        Vector3 xRotation = new Vector3(0f, xRot, 0f) * lookSensitivity;
-        rb.MoveRotation(transform.rotation * Quaternion.Euler(xRotation));
-        //Camera.main.transform.Rotate(xRotation);
 
-        float yRot = Input.GetAxisRaw("Mouse Y");
-        Vector3 yRotation = new Vector3(yRot, 0f, 0f) * lookSensitivity;
+        CameraRotate();
+    }
 
+    public void CameraRotate()
+    {
+        float yRot = Input.GetAxis("Mouse X") * xLookSensitivity;
+        float xRot = Input.GetAxis("Mouse Y") * yLookSensitivity;
 
-        //applies it (if we have settings in the future, we can do inverted here by - yRotatoin).
-        Camera.main.transform.Rotate(yRotation * invertY); 
-        //rb.MoveRotation(transform.rotation * Quaternion.Euler(-yRotation));
+        transform.localRotation *= Quaternion.Euler(0f, yRot, 0f);
+        cam.transform.localRotation *= Quaternion.Euler(-xRot, 0f, 0f);
+        cam.transform.localRotation = ClampRotationAroundXAxis(cam.transform.localRotation);
+
+    }
+
+    //thank u unity
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
     }
 }
